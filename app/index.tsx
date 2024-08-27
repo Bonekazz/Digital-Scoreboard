616e8fa3b9ee341f998cf19d10422ebc8ff3e320
Bonekazz <juracirdantas@gmail.com>
2024-08-27 16:26:51 -0300

    update layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Text, View, StyleSheet, Dimensions, Pressable } from "react-native";
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+const SCORE_TEXT_SIZE = 180;
 
 export default function Index() {
 
@@ -31,22 +33,56 @@ export default function Index() {
   return (
     <View style={{...style.main, flexDirection: (orientation === "portrait" ? "column" : "row")}}>
 
-        <Pressable
+        <View
             style={{...style.teamSite, backgroundColor: teams.redSide.bgColor}}
-            onPress={() => {
-                setTeams({blueSide: {...teams.blueSide}, redSide: {...teams.redSide, score: (teams.redSide.score + 1)}});
-            }}
+
         >
-            <Text style={style.teamSiteText}>{teams.redSide.score}</Text>
-        </Pressable>
-        <Pressable 
+            <View style={{position: "absolute", width: "100%", height: "100%", }}>
+              <Pressable 
+                style={{flex: 1, alignItems: "flex-start", justifyContent: "flex-end", zIndex: 100, width: "100%", height: "50%", top: 0, backgroundColor: "rgba(0, 0, 0, 0.22)"}}
+                onPress={() => {
+                    setTeams({blueSide: {...teams.blueSide}, redSide: {...teams.redSide, score: (teams.redSide.score + 1)}});
+                }}
+              >
+                <FontAwesome name="arrow-up" size={24} color="rgba(255,255,255, 0.21)" style={{paddingLeft: 12, paddingBottom: 12}}/>
+              </Pressable>
+              <Pressable 
+                style={{flex: 1, alignItems: "flex-start", justifyContent: "flex-start", zIndex: 100, width: "100%", height: "50%", top: 0, backgroundColor: "rgba(0, 0, 0, 0.12)"}}
+                onPress={() => {
+                    setTeams({blueSide: {...teams.blueSide}, redSide: {...teams.redSide, score: (teams.redSide.score === 0) ? 0 : teams.redSide.score - 1}});
+                }}
+              >
+                <FontAwesome name="arrow-down" size={24} color="rgba(255,255,255, 0.21)" style={{paddingLeft: 12, paddingTop: 12}}/>
+              </Pressable>
+            </View>
+            <Text style={{...style.teamSiteText, fontSize: (teams.redSide.score > 999) ? 120 : SCORE_TEXT_SIZE}}>{teams.redSide.score}</Text>
+        </View>
+
+        <View
             style={{...style.teamSite, backgroundColor: teams.blueSide.bgColor}}
-            onPress={() => {
-                setTeams({redSide: {...teams.redSide}, blueSide: {...teams.blueSide, score: (teams.blueSide.score + 1)}});
-            }}
+
         >
-            <Text style={style.teamSiteText}>{teams.blueSide.score}</Text>
-        </Pressable>
+            <View style={{position: "absolute", width: "100%", height: "100%", }}>
+              <Pressable 
+                style={{flex: 1, alignItems: "flex-end", justifyContent: "flex-end", zIndex: 100, width: "100%", height: "50%", top: 0, backgroundColor: "rgba(0, 0, 0, 0.22)"}}
+                onPress={() => {
+                    setTeams({redSide: {...teams.redSide}, blueSide: {...teams.blueSide, score: teams.blueSide.score + 1 }});
+                }}
+              >
+                <FontAwesome name="arrow-up" size={24} color="rgba(255,255,255, 0.21)" style={{paddingRight: 12, paddingBottom: 12}}/>
+              </Pressable>
+              <Pressable 
+                style={{flex: 1, alignItems: "flex-end", justifyContent: "flex-start", zIndex: 100, width: "100%", height: "50%", top: 0, backgroundColor: "rgba(0, 0, 0, 0.12)"}}
+                onPress={() => {
+                    setTeams({redSide: {...teams.redSide}, blueSide: {...teams.blueSide, score: (teams.blueSide.score === 0) ? 0 : teams.blueSide.score - 1 }});
+                }}
+              >
+                <FontAwesome name="arrow-down" size={24} color="rgba(255,255,255, 0.21)" style={{paddingRight: 12, paddingTop: 12}}/>
+              </Pressable>
+            </View>
+            <Text style={{...style.teamSiteText, fontSize: (teams.redSide.score > 999) ? 120 : SCORE_TEXT_SIZE}}>{teams.blueSide.score}</Text>
+        </View>
+        
         <Pressable 
             style={style.restartBtn}
             onPress={() => {
@@ -56,7 +92,7 @@ export default function Index() {
                 });
             }}
         >
-            <MaterialCommunityIcons name="restart" size={42} color="black" /> 
+            <FontAwesome name="refresh" size={54} color="white" style={style.restartIcon} /> 
         </Pressable>
     </View>
   );
@@ -79,13 +115,24 @@ const style = StyleSheet.create({
     },
     teamSiteText: {
         color: "white", 
-        fontSize: 64,
+        // fontSize: 180,
         fontWeight: "bold"
     },
     restartBtn: {
         position: "absolute",
-        backgroundColor: "white",
-        borderRadius: 8,
-        padding: 4
+        zIndex: 200,
+        backgroundColor: "rgba(0, 0, 0, 0.21)",
+        borderWidth: 0.5,
+        borderColor: "rgba(255, 255, 255, 0.21)",
+        borderRadius: 100,
+        padding: 12,
+        width: 80,
+        height: 80,
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    restartIcon: {
+      opacity: 0.5 
     }
 });
