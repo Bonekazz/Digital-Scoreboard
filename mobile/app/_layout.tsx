5c4dfa5f5b4ab49ffdeba3ada48ae170747cad72
Bonekazz <juracirdantas@gmail.com>
2024-09-02 17:32:21 -0300

    update folders

--- mobile/app/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Stack } from "expo-router";
-
-export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen options={{headerShown: false}} name="index"/>
-    </Stack>
-  );
-}
